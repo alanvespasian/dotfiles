930b7eacd50831976956c569c3fe9f9c2b85cb38
alanvespasian <145884925+alanvespasian@users.noreply.github.com>
2025-05-15 16:09:08 -0400

    Add files via upload

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -2,11 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
   {"  󰓇   ", "~/.suckless/dwmblocks/scripts/spotify.sh || echo 'Not running'",		5,	0},
-	{"󰋑   ",   "~/.suckless/dwmblocks/scripts/cpu.sh",				10,	0},
-	{"󰋑   ",   "~/.suckless/dwmblocks/scripts/memory.sh",				10,	0},
+	{"󰋑   ",   "~/.suckless/dwmblocks/scripts/cpu.sh",					5,	0},
+	{"󰋑   ",   "~/.suckless/dwmblocks/scripts/memory.sh",			 	5,	0},
 	{"󰕾   ",   "~/.suckless/dwmblocks/scripts/volume.sh",				1,	0},
   {"󰃭   ", "TZ=America/New_York date '+%b %d %a, %Y'", 30, 0},
   {"󰥔   ", "TZ=America/New_York date '+%R'", 5, 0},
+  {"󰁹   ", "~/.suckless/dwmblocks/scripts/battery.sh",                                    10,      0}, 
+	  
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
