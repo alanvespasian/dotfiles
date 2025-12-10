3fa234457f1216b670e9437d8d87d0f5a998292b
alanvespasian <145884925+alanvespasian@users.noreply.github.com>
2025-12-10 17:02:48 -0500

    updated dwmblocks
    better spotify.sh

--- dwmblocks/scripts/spotify.sh
+++ dwmblocks/scripts/spotify.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 # Check if Spotify is running
-if pgrep -x "spotify" > /dev/null; then
-    # Use playerctl to get current song info
-    song_info=$(playerctl metadata artist -f '{{artist}} - {{title}}')
-    if [ -n "$song_info" ]; then
-        echo "$song_info"
-    else
-        echo "Spotify is running, but song info is not available"
-    fi
-else
+if ! pgrep -x "spotify" > /dev/null; then
     echo "Spotify is not running"
+    exit 0
 fi
+
+# If running, fetch metadata normally
+TITLE=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify \
+/org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get \
+string:"org.mpris.MediaPlayer2.Player" string:"Metadata" \
+| grep -A 1 "xesam:title" | tail -n1 | cut -d '"' -f 2)
+
+ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify \
+/org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get \
+string:"org.mpris.MediaPlayer2.Player" string:"Metadata" \
+| grep -A 2 "xesam:artist" | tail -n1 | cut -d '"' -f 2)
+
+echo "$ARTIST - $TITLE"
+
