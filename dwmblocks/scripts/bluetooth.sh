36d57f7e421f23ffc88e529af253a2dc0da7c68d
alanvespasian <145884925+alanvespasian@users.noreply.github.com>
2025-12-11 10:28:55 -0500

    Add files via upload

--- dwmblocks/scripts/bluetooth.sh
+++ dwmblocks/scripts/bluetooth.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-# Bluetooth status for dwmblocks (checks for ANY connected device)
-# Output: Connected / Disconnected
+# Get the connected Bluetooth device name
+name=$(bluetoothctl devices Connected | cut -d ' ' -f 3-)
 
-# Check if Bluetooth is enabled
-if ! bluetoothctl show | grep -q "Powered: yes"; then
-    echo "Off"
-    exit 0
-fi
-
-# Check if ANY device is connected
-if bluetoothctl devices | awk '{print $2}' | while read -r mac; do
-    if bluetoothctl info "$mac" | grep -q "Connected: yes"; then
-        echo "Connected"
-        exit 0  # Exit early if any device is connected
-    fi
-done; then
-    : # Do nothing (exit 0 already printed "Connected")
+if [ -z "$name" ]; then
+    echo "No device connected"
 else
-    echo "Disconnected"
+    echo "$name"
 fi
+
